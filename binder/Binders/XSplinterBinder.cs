ca90ed47bb1d2455b485e14c5e3566cd03657ca3
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2022-05-03 08:58:38 +0100

    Add project files.

---
+++ binder/Binders/XSplinterBinder.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace binder.Binders
+{
+    public class XSplinterBinder:IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            
+           if (!bindingContext.ModelMetadata.IsComplexType)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+                return Task.CompletedTask;
+            };
+
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).ToString();
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                bindingContext.Result = ModelBindingResult.Success(null);
+                return Task.CompletedTask;
+            }
+            var elementType = bindingContext.ModelType.GetTypeInfo();
+            var values = value.Split("x");
+            var x = new TestModel();
+            x.Monkey1 = values[0];
+            x.Monkey2 = values[1];
+            bindingContext.Model = x;
+            bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ binder/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace binder
+{
+    public class TestModel
+    {
+        public string Monkey1 { get; set; }
+        public string Monkey2 { get; set; }
+    }
+}
